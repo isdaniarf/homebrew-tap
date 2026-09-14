586033daf3f09d4b5e71db641618a1bb52cfd56c
Dani Fithrantyo <fithrantyo@gmail.com>
2026-09-14 11:58:19 +0700

    fix: use the symbol form for the macOS dependency

--- Casks/kafkamitter.rb
+++ Casks/kafkamitter.rb
@@ -8,7 +8,7 @@ cask "kafkamitter" do
   homepage "https://github.com/isdaniarf/homebrew-tap"
 
   depends_on arch: :arm64
-  depends_on macos: ">= :ventura"
+  depends_on macos: :ventura
 
   app "Kafkamitter.app"
 
